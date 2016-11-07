f4d65aaa4c3bc765a9212d6ef09ab41455d43a78
Jon Madden <jmadden@incomm.com>
2016-11-07 13:27:28 -0800

    Adding models

---
+++ MT.JsonLoader/MT.JsonLoader/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MT.JsonLoader.Models
+{
+    public class Character
+    {
+        public Guid Id { get; set; }
+        public string CharacterRace { get; set; }
+        public string CharacterClass { get; set; }
+    }
+}
---
+++ MT.JsonLoader/MT.JsonLoader/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MT.JsonLoader.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Followers { get; set; }
+    }
+}
