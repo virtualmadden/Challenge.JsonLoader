8210504fa3dd21a97fc9fea668c2835c6ee7ca81
Jon Madden <jmadden@incomm.com>
2016-11-07 16:13:28 -0800

    creating models and playing around with combing them.

--- MT.JsonLoader/MT.JsonLoader/Models/Character.cs
+++ MT.JsonLoader/MT.JsonLoader/Models/Character.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MT.JsonLoader.Models
 {
     public class Character
     {
+        [JsonProperty("Id")]
         public Guid Id { get; set; }
+
+        [JsonProperty("CharacterRace")]
         public string CharacterRace { get; set; }
+
+        [JsonProperty("CharacterClass")]
         public string CharacterClass { get; set; }
     }
 }
--- MT.JsonLoader/MT.JsonLoader/Models/Person.cs
+++ MT.JsonLoader/MT.JsonLoader/Models/Person.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MT.JsonLoader.Models
 {
     public class Person
     {
+        [JsonProperty("Id")]
         public Guid Id { get; set; }
+
+        [JsonProperty("FirstName")]
         public string FirstName { get; set; }
+
+        [JsonProperty("LastName")]
         public string LastName { get; set; }
+
+        [JsonProperty("Email")]
         public string Email { get; set; }
-        public int Followers { get; set; }
+
+        [JsonProperty("CharacterRace")]
+        public string CharacterRace { get; set; }
+
+        [JsonProperty("CharacterClass")]
+        public string CharacterClass { get; set; }
+
+        public Followers FollowerCount { get; set; }
+
+        public string SourceFile { get; set; }
     }
 }
