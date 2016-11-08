0430f2dedf5cf491c5f9c047c93c31a9ac71d287
Jon Madden <jmadden@incomm.com>
2016-11-07 16:15:54 -0800

    Adding load function to program

--- MT.JsonLoader/MT.JsonLoader/Program.cs
+++ MT.JsonLoader/MT.JsonLoader/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MT.JsonLoader.Core;
+using MT.JsonLoader.Models;
 
 namespace MT.JsonLoader
 {
@@ -14,10 +17,25 @@ namespace MT.JsonLoader
             //  Starts app
             UserCommand userCommand;
             var commandHandler = new CommandHandler();
+
+            var people = LoadJsonFiles().ToList();
+
             do
             {
-                userCommand = commandHandler.ReadCommand();
+               userCommand = commandHandler.ReadCommand(people);
             } while (!userCommand.IsExit);
+        }
+
+        private static IEnumerable<Person> LoadJsonFiles()
+        {
+            var files = FileHandler.GetFiles(@"..\..\Data");
+
+            var people = new List<Person>();
+
+            foreach (var file in files)
+                people.AddRange(FileHandler.LoadFile(file));
+
+            return people;
         }
     }
 }
