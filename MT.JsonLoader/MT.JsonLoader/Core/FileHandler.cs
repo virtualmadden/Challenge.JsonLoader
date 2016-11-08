3cb60714681861d437ebd0dcbba94d22cc2d9f68
Jon Madden <jmadden@incomm.com>
2016-11-07 16:14:03 -0800

    Adding a file handler class.

---
+++ MT.JsonLoader/MT.JsonLoader/Core/FileHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MT.JsonLoader.Models;
+using Newtonsoft.Json;
+
+namespace MT.JsonLoader.Core
+{
+    public static class FileHandler
+    {
+        public static List<string> GetFiles(string directory)
+        {
+            return Directory.GetFiles(directory).Select(Path.GetFileName).ToList();
+        }
+
+        public static List<Person> LoadFile(string fileName)
+        {
+            using (var reader = new StreamReader($@"..\..\Data\{fileName}"))
+            {
+                var content = reader.ReadToEnd();
+
+                var people = JsonConvert.DeserializeObject<List<Person>>(content, new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore});
+
+                people.ForEach(x => { x.SourceFile = fileName; });
+
+                return people;
+            }
+        }
+    }
+}
